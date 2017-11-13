0168c5f45d3ca3a78ece20f0d5d6aefa8137fcd7
Ari <contact@blangkon.com>
2017-11-13 11:49:22 +0700

    Add new archetype : rest service using SpringBoot

---
+++ archetype-rest-springboot/src/main/resources/archetype-resources/src/main/java/JSONController.java
@@ -0,0 +1,19 @@
+package ${package};
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JSONController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+	
+}
